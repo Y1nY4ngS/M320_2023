b18d168788ab81b567caf9f54fda1d00f93ca6d6
Quoc Hung Nguyen <quochungnguyen1105@gmail.com>
2023-08-27 22:40:51 +0200

    haus

--- Übungsaufgabe 1/MyMath.cs
+++ Übungsaufgabe 1/MyMath.cs
@@ -8,5 +8,65 @@ namespace Übungsaufgabe_1
 {
     internal class MyMath
     {
+        static int Calc_ggT(int a, int b)
+        {
+            int z;
+            while (b != 0)
+            {
+                z = a % b;
+                a = b;
+                b = z;
+            }
+            return a;
+        }
+
+        static int Calc_kgV(int a, int b)
+        {
+            return (a * b) / Calc_ggT(a, b);
+        }
+
+        static int ReadInt()
+        {
+            int i;
+            do
+            {
+                Console.WriteLine("Bitte Zahl eingeben");
+                string s = System.Console.ReadLine();
+                int.TryParse(s, out i);
+            } while (i <= 0);
+            Console.WriteLine("danke");
+            return i;
+        }
+
+        static void ShowResult(string op, int a, int b, int c)
+        {
+            Console.WriteLine(op + " von {0} und {1} ist {2}", a, b, c);
+        }
+
+        private static string MenuOeffnen()
+        {
+
+            Console.WriteLine("");
+            Console.WriteLine("Bitte Operation eingeben:");
+            Console.WriteLine("  g für ggT");
+            Console.WriteLine("  k für kgV");
+            Console.WriteLine("  r für ggTr");
+            Console.WriteLine("  e für exit");
+            string de = Console.ReadLine();
+            return de;
+
+        }
+
+        static int Calc_ggT_r(int a, int b)
+        {
+            if (b == 0)
+            {
+                return a;
+            }
+            else
+            {
+                return Calc_ggT_r(b, a % b);
+            }
+        }
     }
 }
--- Übungsaufgabe 1/Program.cs
+++ Übungsaufgabe 1/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace Übungsaufgabe_1
+﻿using System.ComponentModel.Design;
+
+namespace Übungsaufgabe_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                string de = MenuOeffnen();
+                if (IstDe(de))
+                {
+                    int a = IntRead();
+                    int b = IntRead();
+                    int r;
+                    switch (de)
+                    {
+                        case "g":
+                            r = Calc_ggT(a, b);
+                            ShowResult("ggT", a,b, r);
+                            break;
+                        case "k":
+                            r = Calc_kgV(a, b);
+                            ShowResult("kgV", a, b, r);
+                            break;
+                        case "r":
+                            r = Calc_ggT_r(a, b);
+                            ShowResult("ggTr", a, b, r);
+                            break;
+                        default:
+                            Console.WriteLine("Operation geht nicht");
+                            break;
+                    }
+                } else if (de== 'e')
+                {
+                    Console.WriteLine("Aufwiedersehen");
+                    break;
+                }
+            }
+            private static bool IsValidOp(string op)
+            {
+                return op == "g" || op == "k" || op == "r";
+            }
         }
     }
 }
